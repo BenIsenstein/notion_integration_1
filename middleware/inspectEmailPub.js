518f2d54c10fad3f0258c8795000eaea07bbc930
Ben Isenstein <ben.isenstein@gmail.com>
2022-11-11 01:04:09 -0500

    fix(middleware): temporarily disable

--- middleware/inspectEmailPub.js
+++ middleware/inspectEmailPub.js
@@ -3,28 +3,28 @@ const gmail = google.gmail('v1')
 const { articlePubsubLogs, close } = require('../repositories')
 
 module.exports.inspectEmailPub = async (req, res, next) => {
-    try {
-        const data = req.body?.message?.data
-        const id = req.body?.message?.messageId
+    // try {
+    //     const data = req.body?.message?.data
+    //     const id = req.body?.message?.messageId
 
-        if (!data) {
-            res.status(500).send('No message data')
-            return
-        }
+    //     if (!data) {
+    //         res.status(500).send('No message data')
+    //         return
+    //     }
 
-        const dataJson = Buffer.from(data, 'base64').toString()
-        const { emailAddress: userId } = JSON.parse(dataJson)
-        const messageRes = await gmail.users.messages.get({ userId, id })
-        const { headers } = messageRes.data.payload
-        const subject = headers.find(h => h.name === 'Subject')?.value?.slice(0,96)
-        const from = headers.find(h => h.name === 'From')?.value
-        const datetime = Intl.DateTimeFormat('en-US', { dateStyle: 'short', timeStyle: 'long' }).format()
+    //     const dataJson = Buffer.from(data, 'base64').toString()
+    //     const { emailAddress: userId } = JSON.parse(dataJson)
+    //     const messageRes = await gmail.users.messages.get({ userId, id })
+    //     const { headers } = messageRes.data.payload
+    //     const subject = headers.find(h => h.name === 'Subject')?.value?.slice(0,96)
+    //     const from = headers.find(h => h.name === 'From')?.value
+    //     const datetime = Intl.DateTimeFormat('en-US', { dateStyle: 'short', timeStyle: 'long' }).format()
 
-        await articlePubsubLogs.insertOne({ id, datetime, from, subject })
-        close()
-    } catch (err) {
-        console.log(err)
-    }
-    
+    //     await articlePubsubLogs.insertOne({ id, datetime, from, subject })
+    //     close()
+    // } catch (err) {
+    //     console.log(err)
+    // }
+
     next()
 }
