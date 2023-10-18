45193ca9e541dee18d392cd6dcac3f1c33dbd151
Ben Isenstein <ben.isenstein@gmail.com>
2023-10-18 14:11:44 -0600

    fix: queue service url

--- src/services/queue.service.ts
+++ src/services/queue.service.ts
@@ -2,7 +2,7 @@ import { HttpJob } from '../types'
 import { queueAxios } from '../repositories/queue.repository'
 
 export const createHttpJob = async (job: HttpJob) => {
-    const res = await queueAxios.post('/jobs', job)
+    const res = await queueAxios.post('/', job)
     console.log('creating new http job:', job.method, job.url)
     console.log('queue service res:', res.status, res.statusText)
 }
