0f5a98eca61f3789379f292f66c10a0da02b9b60
Ben Isenstein <ben.isenstein@gmail.com>
2023-11-01 14:29:15 -0600

    chore: observe tokens storage

--- src/helpers/initGoogleApi.ts
+++ src/helpers/initGoogleApi.ts
@@ -5,7 +5,12 @@ import { USER_ID } from '../values'
 export const initGoogleApi = async () => {
     const tokens = await getGoogleTokens(USER_ID)
 
-    if (tokens) oauth2Client.setCredentials(tokens)
+    if (tokens) {
+        console.log('found stored tokens in SQLite: ', JSON.stringify(tokens, null, ' '))
+        oauth2Client.setCredentials(tokens)
+    } else {
+        console.log('WARNING, no tokens stored in SQLite')
+    }
     
     google.options({ auth: oauth2Client })
 }
